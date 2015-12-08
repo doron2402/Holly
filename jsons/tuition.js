a7f5139501c235337da83e74bc2ec7afc41696c6
Doron Segal <doron2402@gmail.com>
2015-12-07 20:25:16 -0800

    working on next year tuition

--- jsons/tuition.js
+++ jsons/tuition.js
@@ -1,5 +1,79 @@
 var tuitionContext = {
   "header": "Tuition for 2015-16",
+  "nextYear": true,
+  "nextYearLink": {
+    "text": "TUITION FOR 2016-17 press here"
+  },
+  "footer": "In addition there is a $400 supply fee due each year on September 1.",
+  "items": [
+    {
+      "header": "3 Days a Week",
+      "sub_header": "",
+      "sub_items": [
+        {
+          "text": "8:00 - 9:00 $100",
+        },
+        {
+          "text": "9:00-12:45 $1250"
+        },
+        {
+          "text": "9:00-3:00 $1275"
+        },
+        {
+          "text": "3:00-5:30 $150"
+        },
+        {
+          "text": "* 8:00-5:30 $1525"
+        }
+      ]
+    },
+    {
+      "header": "4 Days a Week",
+      "sub_header": "",
+      "sub_items": [
+        {
+          "text": "8:00 - 9:00 $100",
+        },
+        {
+          "text": "9:00-12:45 $1350"
+        },
+        {
+          "text": "9:00-3:00 $1450"
+        },
+        {
+          "text": "3:00-5:30 $150"
+        },
+        {
+          "text": "* 8:00-5:30 $1700"
+        }
+      ]
+    },
+    {
+      "header": "5 Days a Week",
+      "sub_header": "",
+      "sub_items": [
+        {
+          "text": "8:00 - 9:00 $100",
+        },
+        {
+          "text": "9:00-12:45 $1475"
+        },
+        {
+          "text": "9:00-3:00 $1575"
+        },
+        {
+          "text": "3:00-5:30 $150"
+        },
+        {
+          "text": "* 8:00-5:30 $1750"
+        }
+      ]
+    }
+  ]
+};
+
+var tuitionContextNextYear = {
+  "header": "Tuition for 2016-17",
   "footer": "In addition there is a $400 supply fee due each year on September 1.",
   "items": [
     {
